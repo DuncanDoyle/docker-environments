00da68262b9373521e38681182f111714a3a66c0
Duncan Doyle <Duncan.Doyle@gmail.com>
2014-12-18 14:51:56 +0100

    Added jboss-eap-6.3.2 image.

---
+++ jboss/jboss-eap/jboss-eap-6.3.2/build-container.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#
+# Builds the Docker container.
+#
+# author: ddoyle@redhat.com
+#
+docker build --rm -t ddoyle/jboss-eap:6.3.2 .
---
+++ jboss/jboss-eap/jboss-eap-6.3.2/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Seting up the Docker directory."
+echo "Copying JBoss EAP patch script."
+cp ../../../scripts/patch-jboss-eap.sh dockerfile_copy/
