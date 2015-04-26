2240134ae23b7553bd34b77474085977265bfddd
Duncan Doyle <Duncan.Doyle@gmail.com>
2015-04-26 23:04:37 +0200

    Dynamically added repos are now added to public Nexus repository group.

--- nexus/nexus-server/dockerfile_copy/add_nexus_repositories.sh
+++ nexus/nexus-server/dockerfile_copy/add_nexus_repositories.sh
@@ -3,12 +3,16 @@
 # This scripts is used in our Nexus Docker containers to scan for any json files with a request to configure a new repository on the Nexus server.
 #
 # The main idea is that our nexus-data container, from which we mount the volume in our nexus-server containet, contains these json files. When we start our nexus container,
-# we first wait until the nexus repo is available (we check whether the REST API is available), next we check which scripts we've already executed (we keep a small cache in our container).
-# This allows us to control not to run scripts twice if we re-use the container (in docker-compose, containers are always rebuild, so we won't have that problem, but this
-# is to support additional use-cases as well).
+# we first wait until the nexus repo is available (we check whether the REST API is available). Next, we search the $JSON_ADD_REPO_REQUESTS_DIR for JSON request files to fire
+# against the Nexus REST API to add repositories.
+#
+# Finally, we add all repositories to the 'public' group of Nexus so we can actually access them via the public repository URL.
 #
 # This allows us to plugin new data-containers with new Maven repo's without altering the nexus-server container. Also, it allows us to upgrade the nexus-server container, without 
 # having to rebuild the data-container.
+
+# TODO: We might want to check which scripts we've already executed (we keep a small cache in our container). This would allow us to control not to run scripts twice if we re-use the container.
+# (in docker-compose, containers are always rebuild, so we won't have that problem, but this is to support additional use-cases as well).
 #
 # author: ddoyle@redhat.com
 #
@@ -90,6 +94,7 @@ else
 fi
 
 # Now, loop through all the JSON files that we want to execute against the repo.
+ADD_REPOS_TO_PUBLIC_GROUP_FILE="add_repos_to_public.txt"
 if [ ! -z "$JSON_ADD_REPO_REQUESTS_DIR" ]
 then
 # Now that the system is running, we can run our CLI scripts. They will be executed in the order defined by 'sort -V'
@@ -99,8 +104,40 @@ then
 	for f in `find $JSON_ADD_REPO_REQUESTS_DIR -type f -name "*_add_request.json"`
         do
                 echo "Calling Nexus REST API. Adding repositories. JSON request file: " $f
-		curl -i -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -v -d "@$f" -u "admin:admin123" "http://localhost:8081/nexus/service/local/repositories"			
+		curl -i -H "Accept: application/json" -H "Content-Type: application/json" -f -X POST  -v -d "@$f" -u "admin:admin123" "http://localhost:8081/nexus/service/local/repositories"
+		# We now add the ID of the repo to a temp file so we can add the repo automatically to the 'public' group of Nexus.
+		cat $f | /usr/local/bin/jq ".data.id" >> $ADD_REPOS_TO_PUBLIC_GROUP_FILE
         done
 fi
+
+# We now parse our temp file with repo IDs and configure the Nexus public repo.
+echo "Adding new repositories to Nexus public group."
+UPDATE_PUBLIC_GROUP_JSON="update-public-group.json"
+
+echo '{"data":
+        {"id":"public",
+        "name":"Public Repositories",
+        "format":"maven2",
+        "exposed":true,
+        "provider":"maven2",
+        "repositories":[
+                {"id":"releases"},
+                {"id":"snapshots"},
+                {"id":"thirdparty"},
+                {"id":"central"}' > $UPDATE_PUBLIC_GROUP_JSON
+
+while read repo
+do
+   echo "               ,{\"id\": $repo}" >> $UPDATE_PUBLIC_GROUP_JSON
+done < $ADD_REPOS_TO_PUBLIC_GROUP_FILE
+
+echo '  ]}
+}' >> $UPDATE_PUBLIC_GROUP_JSON
+
+#Execute this REST call to update the public group:
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" -f -X PUT  -v -d "@$UPDATE_PUBLIC_GROUP_JSON" -u "admin:admin123" "http://localhost:8081/nexus/service/local/repo_groups/public"
+
+#And remove the temp file with repo's.
+#rm $UPDATE_PUBLIC_GROUP_JSON && rm $ADD_REPOS_TO_PUBLIC_GROUP_FILE
 
 echo -e "\n\nDone"
